d66029b5842cc00606a9edf609636a52fe1c2674
takamooori <yuhei.takamori.3i@stu.hosei.ac.jp>
2026-05-26 08:03:26 +0000

    feat: LiDAR2 対応のセグメントプログラム追加です。

--- pcd_convert/setup.py
+++ pcd_convert/setup.py
@@ -47,7 +47,8 @@ setup(
             'pcd_rotation = pcd_convert.pcd_rotation:main',
             'pcd_height_segmentation = pcd_convert.pcd_height_segmentation:main',
             'pcd_reflect_segmentation = pcd_convert.pcd_reflect_segmentation:main',
-            'pcd_rotation_lidar2 = pcd_convert.pcd_rotation_lidar2:main'
+            'pcd_rotation_lidar2 = pcd_convert.pcd_rotation_lidar2:main',
+            'pcd_height_segmentation_lidar2 = pcd_convert.pcd_height_segmentation_lidar2:main'
         ],
     },
 )
